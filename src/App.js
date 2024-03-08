1682d117436c595cb8cd8ce451bf727f952640f6
Tanushree Singh <tanushreesingh@Tanushrees-MacBook-Air.local>
2024-03-08 00:48:46 -0800

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {"Hello World"}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
     </div>
   );
 }
